c2b588d47cb8f9c6021ccd2d34afe7637514efa8
hossein.oudaran <hossein.oudaran@gmail.com>
2025-02-10 14:22:09 +0330

    change some styles

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,8 +3,8 @@ import LoginForm from "../features/login/loginForm";
 
 function LoginPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-primary-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm">
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="bg-secondary-50 p-6 rounded-lg shadow-lg w-full max-w-sm">
         <h2 className="text-2xl font-bold mb-4">ورود به سیستم</h2>
         <LoginForm />
       </div>
