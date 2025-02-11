0cbf08d50601793d6da34171268c6aafba800aa3
hossein.oudaran <hossein.oudaran@gmail.com>
2025-02-11 08:20:23 +0330

    change some file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     external: ['./features/Baristas/BaristaShiftForm']
-  //   }
-  // }
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: []
+    }
+  }
 });
